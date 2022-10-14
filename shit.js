56993b70bf2f994c81eeb52febad216049c8fab4
Kiril Misnikov <56405352+lime360@users.noreply.github.com>
2022-10-14 06:44:58 +0000

    yo omg i did a cool trick
    I JUST MERGED CONSOLE.LOG() WITH ALERT()

--- shit.js
+++ shit.js
@@ -1,2 +1,7 @@
-const your-mom = 69420
-const xd = Math.PI * your-mom
+const your-mom = 69420;
+const xd = Math.PI * your-mom;
+const output = function() {
+   console.log(text)
+   alert(text)
+};
+output(xd);
