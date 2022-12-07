c37e1803e0402db01c685dd3521cf7814d34b2bd
lime360 <emeraldlime@protonmail.com>
2022-12-07 22:02:02 +0200

    added another python script

---
+++ setvalues.py
@@ -0,0 +1,20 @@
+#! /usr/bin/python
+
+testVar = 5
+
+def changeValue(type, value):
+    if type == "addValue":
+        print(testVar + value)
+    elif type == "subtractValue":
+        print(testVar - value)
+    elif type == "setValue":
+        print(testVar = value)
+
+print("ok im gonna demonstarate a funny custom function i made in python")
+
+print(testVar)
+
+changeValue("addValue", 40)
+
+# value 1: type of the value changine process (can be addValue, subtractValue or setValue)
+# value 2: number
