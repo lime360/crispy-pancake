9d498a73f76f6182a64a273c42239d2f204f8cc1
lime360 <emeraldlime@protonmail.com>
2022-12-23 21:39:57 +0200

    made something lol

---
+++ whattimeisit.py
@@ -0,0 +1,8 @@
+#! /usr/bin/python
+
+from random import shuffle
+
+yes = ["morbin", "minionin", "gruin", "arienain", "sweepin", "timin", "rapin", "placeholderin"]
+shuffle(yes)
+
+print("it's " + yes[1] + " time")
